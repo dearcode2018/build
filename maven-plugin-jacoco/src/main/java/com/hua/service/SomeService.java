d2a989431be09db669401f294300dd294ac21f24
qianye.zheng <a>
2021-03-18 11:29:33 +0800

    no message

---
+++ maven-plugin-jacoco/src/main/java/com/hua/service/SomeService.java
@@ -0,0 +1,20 @@
+/**
+  * @filename SomeService.java
+  * @description 
+  * @version 1.0
+  * @author qianye.zheng
+ */
+package com.hua.service;
+
+ /**
+ * @type SomeService
+ * @description 
+ * @author qianye.zheng
+ */
+public final class SomeService {
+
+	public void print() {
+		System.out.println("SomeService.print()");
+	}
+	
+}
